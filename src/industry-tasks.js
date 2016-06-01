ee653c515f302afe6958dc7474aa0bed18b27846
Winton Welsh <mail@wintoni.us>
2016-06-01 00:02:17 -0700

    Call run with normal args

--- src/industry-tasks.js
+++ src/industry-tasks.js
@@ -43,11 +43,14 @@ function showTasks() {
 export let tasks = Class =>
   class extends Class {
 
-    run({ argv=process.argv.slice(2) }) {
-      argv = parseArgv(argv)
+    run({ args, argv }) {
+      if (argv) {
+        delete args.argv
+        args = parseArgv(argv)
+      }
 
       let factory, task
-      let { _ } = argv
+      let { _ } = args
       let tasks = _.indexOf("tasks") > -1 || _.indexOf("t") > -1
       
       if ([ "tasks", "t" ].indexOf(_[0]) == -1) {
@@ -66,8 +69,8 @@ export let tasks = Class =>
       if (tasks) {
         return showTasks.bind(factory)()
       } else if (task) {
-        return factory.run({ ...argv, _ })
-      } else {
+        return factory.run({ ...args, _ })
+      } else if (super.run) {
         return super.run({ ...argv, _ })
       }
     }
